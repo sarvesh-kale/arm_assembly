0190daf4d9895d5667e2910b7c1015e77a8b06c6
sarvesh-kale <sarveshkale7217@gmail.com>
2022-11-26 23:57:27 +0530

    wrote print and scanf:wq

--- functions/ex1.s
+++ functions/ex1.s
@@ -1,4 +1,8 @@
-.data 
+.data
+.balign 4
+msg: .asciz "input two numbers :- \n"
+.balign 4
+recv : .asciz "%d%d"
 .balign 4
 return : .word 0 
 .balign 4
@@ -12,4 +16,20 @@ main :
 	@ call scanf and receive 2 nums 
 	@ print their sum 
 	@ restore lr 
+	ldr r1,=return 
+	str lr,[r1]
+	
+	@print msg 
+	ldr r0, =msg 
+	bl printf
+
+	@call scanf (prepare inputs before) 	
+	ldr r0, =msg 
+	ldr r1, =in1
+	ldr r2, =in2 
+	bl scanf 
+
+	@print their sum 
+.glbal printf 
+.global scanf 
 
