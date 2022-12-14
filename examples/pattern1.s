acf704bbd4c68e0df88c703f2e3a62e45a367cd0
sarvesh-kale <sarveshkale7217@gmail.com>
2022-12-14 23:24:37 +0530

    made printing the triangle pattern generalized, takes input from user number of lines

--- examples/pattern1.s
+++ examples/pattern1.s
@@ -1,24 +1,44 @@
 @ this code prints a triangle pattern 
 @ Write code to print the triangle for first 5 lines and generalize next 
 @ 
-.data 
+.data @ data section of assembly 
 .balign 4
 star: .asciz "*"
 .balign 4
 nxtLine: .asciz "\n"
+.balign 4
+takeinput: .asciz "%d"
+.balign 4
+displaytext: .asciz "Enter the number of lines you want the pattern to be printed \n"
+.balign 4
+number : .word 0
 
-.text 
+.text @ the code section  
 .global main 
 main :
 	@ save lr 
 	push {lr}
 
+	@ print msg
+	ldr r0, =displaytext 
+	bl printf 
+
+	@ read the input and use in the following for loop section 
+	ldr r0, =takeinput
+	ldr r1, =number 
+	bl scanf 
+	
+	@ r1 has the address of the number of lines to be printed 
+	ldr r1, =number @ load the addresses again as scanf modifies the r0 and r1 register 
+	ldr r2, [r1]
+	mov r7, r2      @ r7 contains the number of lines to be printed 
+
 	@ init var for looping
 	mov r5, #1 @ using r5 as loop variable as it is not modified by return of printf
-
+	
 	@ call print on star as many times as line number
 	loop1:
-		cmp r5, #100       @ this statement has to be changed to make generalized 
+		cmp r5, r7       @ this statement has to be changed to make generalized 
 		bgt endloop1
 		mov r6,#1
 		
