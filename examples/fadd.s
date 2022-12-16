45f8afd2a0f36a5499d26316930ef1cb1ab00726
sarvesh-kale <sarveshkale7217@gmail.com>
2022-12-16 20:42:09 +0530

    able to take inputs as float but cannot display it, correct output observed in  in gdb

--- examples/fadd.s
+++ examples/fadd.s
@@ -1,18 +1,23 @@
 .data @data section
 
 .balign 4
-read: .asciz "%f%f"
+read: .string "%f%f"
 .balign 4
-output : .asciz "%f+%f=%f\n"
+input : .string "Enter two float numbers \n"
 .balign 4
-num1: .float 0
+num1: .word 0
 .balign 4
-num2: .float 0
-
+num2: .word 0
+.balign 4
+num3: .word 0
 .text @ code secion  
 .global main 
 main :
 		push {lr}
+		
+		ldr r0, =input 
+		bl printf 
+
 		ldr r0, =read @ read the floating point numbers 
 		ldr r1, =num1 
 		ldr r2, =num2 
@@ -25,11 +30,9 @@ main :
 		vldr s2, [r3]
 		
 		
-		vadd.f32 s3, s1, s2 @ do floating point add
-		vmov r3, s3     @ mov result back 
+		vadd.f32 s1, s2 @ do floating point add
 
-		ldr r0, =output 
-		bl printf 
+		@ examine s1 in gdb it will contain the result of addition 	
 
 		pop {lr}            @ restore lr  
 		bx lr @ end  
